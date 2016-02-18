6946a5349dbc220713abff0cd07102fe8b0d2470
Leilei <leilei@LeileideMacBook-Pro.local>
2016-02-18 16:34:23 -0500

    okhttp added

--- app/src/main/java/jian/zhang/oceanwithlibrarys/OceanModule.java
+++ app/src/main/java/jian/zhang/oceanwithlibrarys/OceanModule.java
@@ -7,6 +7,8 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import jian.zhang.oceanwithlibrarys.constants.Constants;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -23,12 +25,17 @@ public class OceanModule {
     @Singleton
     @Provides
     public OceanAPI provideOceanAPI() {
+        int cacheSize = 10 * 1024 * 1024; // 10 MiB
+        Cache cache =  new Cache(context.getCacheDir(), cacheSize);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cache(cache)
+                .build();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.OCEAN_CANDY_BASE_URL2)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
         return retrofit.create(OceanAPI.class);
-
     }
 }
